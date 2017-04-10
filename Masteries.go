33196ae99f35863a2f98cb2aeb0407da295077b9
Djipi <ghislain.piot@free.fr>
2017-04-10 17:51:54 +0200

    Added Masteries V3 endpoint

--- Masteries.go
+++ Masteries.go
@@ -1,6 +1,37 @@
 package gololapi
 
+import (
+	"encoding/json"
+	"strconv"
+	"time"
+)
+
 type Mastery struct {
 	ID   int `json:"masteryId"`
 	Rank int
+}
+
+type MasteryPages struct {
+	Pages []MasteryPage
+	ID    int
+}
+type MasteryPage struct {
+	Current   bool
+	Masteries []Mastery
+	Name      string
+	ID        float64
+}
+
+func (s *Summoner) GetMasteryPages() (pages MasteryPages, e error) {
+	response, e := s.API.RequestEndpoint("/lol/platform/v3/masteries/by-summoner/"+strconv.Itoa(s.ID), time.Hour)
+	if e != nil {
+		panic(e)
+	}
+	pages = MasteryPages{}
+	err := json.Unmarshal(response, &pages)
+	if err != nil {
+		panic(err)
+	}
+
+	return
 }
