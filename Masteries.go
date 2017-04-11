79a8009a1df221d2254c49e9357b5b2d35e26ff6
Djipi <ghislain.piot@free.fr>
2017-04-11 15:45:10 +0200

    Added comments.

--- Masteries.go
+++ Masteries.go
@@ -6,15 +6,19 @@ import (
 	"time"
 )
 
+//Mastery Holds the ID and Rank from a mastery.Is obtained via the Masteries-V3 endpoint
 type Mastery struct {
 	ID   int `json:"masteryId"`
 	Rank int
 }
 
+//MasteryPages Holds the mastery pages of a summoner in Pages and his summoner ID in ID.
 type MasteryPages struct {
 	Pages []MasteryPage
 	ID    int
 }
+
+//MasteryPage Holds the data of a mastery page.
 type MasteryPage struct {
 	Current   bool
 	Masteries []Mastery
@@ -22,7 +26,8 @@ type MasteryPage struct {
 	ID        float64
 }
 
-func (s *Summoner) GetMasteryPages() (pages MasteryPages, e error) {
+//GetMasteryPages Makes a request to the Masteries-V3 endpoint and returns a MasteryPages struct
+func (s *Summoner) GetMasteryPages() (pages MasteryPages) {
 	response, e := s.API.RequestEndpoint("/lol/platform/v3/masteries/by-summoner/"+strconv.Itoa(s.ID), time.Hour)
 	if e != nil {
 		panic(e)
--- Runes.go
+++ Runes.go
@@ -6,26 +6,34 @@ import (
 	"time"
 )
 
+//Rune Holds the data of a rune .To get more information about the rune, you need to query the Static Data.
 type Rune struct {
 	Count int
 	ID    int `json:"runeId"`
 }
+
+//RunePages Holds the runes pages of a summoner and his summoner ID.
 type RunePages struct {
 	Pages []RunePage
 	ID    int
 }
+
+//RunePage Holds the data of a rune page.The runes are in Slots.
 type RunePage struct {
 	Current bool
 	Slots   []RuneSlot
 	Name    string
 	ID      int
 }
+
+//RuneSlot Holds the data of a rune slot.To get more information about the rune, you need to query the Static Data.
 type RuneSlot struct {
 	RuneSlotID int
-	RuneID     int
+	ID         int `json:"runeId"`
 }
 
-func (s *Summoner) GetRunePages() (pages RunePages, e error) {
+//GetRunePages Makes a request to the Runes-V3 endpoint and returns a RunePages struct
+func (s *Summoner) GetRunePages() (pages RunePages) {
 	response, e := s.API.RequestEndpoint("/lol/platform/v3/runes/by-summoner/"+strconv.Itoa(s.ID), time.Hour)
 	if e != nil {
 		panic(e)
